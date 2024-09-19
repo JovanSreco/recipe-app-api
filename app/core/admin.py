4c8ee9a4b036c72743d0422a16745773154b548c
JovanSreco <jovan.sreco@gmail.com>
2024-09-19 12:43:31 +0200

    flake8-fixes

--- app/core/admin.py
+++ app/core/admin.py
@@ -7,6 +7,7 @@ from django.utils.translation import gettext_lazy as _
 
 from core import models
 
+
 # This is optional configuration for django admin
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
@@ -42,6 +43,7 @@ class UserAdmin(BaseUserAdmin):
             ),
         }),
     )
+
 
 # can run without UserAdmin
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -10,7 +10,7 @@ from django.test import Client
 class AdminSiteTests(TestCase):
     """Tests for Django admin."""
 
-    # Need this camel case because of python naming conventions, this test will runfirst before any other test
+    # Need this camel case because of python naming conventions
     def setUp(self):
         """Create user and client."""
         self.client = Client()
@@ -32,12 +32,14 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
     def test_edit_user_page(self):
         """Test the edit user page works."""
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
+
     def test_create_user_page(self):
         """Test the create user page works."""
         url = reverse('admin:core_user_add')
